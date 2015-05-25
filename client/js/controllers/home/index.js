79e6bb42ad6be16d91afecb7346d71cc156186d2
Mok Ka Chun <mkc188@users.noreply.github.com>
2015-05-25 19:03:54 +0800

    Start over from generator-modern-frontend

--- client/js/controllers/home/index.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports = function (model, container, route) {
-  console.log('Rendered view %s using model:\n%s', route.action, JSON.stringify(model, null, 2));
-};
--- client/js/main.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var taunus = require('taunus');
-var wiring = require('../../.bin/wiring');
-var main = document.getElementsByTagName('main')[0];
-
-taunus.mount(main, wiring);
